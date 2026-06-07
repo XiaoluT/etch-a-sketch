38386bf828fbdf3baaf1e0c7a8c0227120d26d1b
Xiaolu Tan <xiaolu.tan2025@outlook.com>
2026-06-07 14:36:13 +0100

    feat: create 16x16 sketch grid with hover effect

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector("#container")
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+
+    square.classList.add("square");
+
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    })
+
+    container.appendChild(square);
+}
